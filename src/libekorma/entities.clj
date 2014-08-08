3af8c4efd08b9bf759f5f9c55e079679d6e093c8
Alex Turok <a.o.turok@gmail.com>
2014-08-09 00:52:16 +0400

    Added tasktag entity and m-n relationship + tasktag insertion

--- src/libekorma/entities.clj
+++ src/libekorma/entities.clj
@@ -1,6 +1,8 @@
 (ns libekorma.entities
 	(:use [korma.core]))
 
+(declare tag)
+
 (defentity task
 	(pk :task_id)
 	(entity-fields :task_id
@@ -9,9 +11,14 @@
 				   :is_done
 				   :is_cancelled
 				   :created_time
-				   :finished_time))
+				   :finished_time)
+	(many-to-many tag :tasktag))
 
 (defentity tag
 	(pk :tag_id)
 	(entity-fields :tag_id
-				   :tag))
+				   :tag)
+	(many-to-many task :tasktag))
+
+(defentity tasktag
+	(entity-fields :task_id :tag_id))
--- src/libekorma/sample.clj
+++ src/libekorma/sample.clj
@@ -20,3 +20,22 @@
 			 {:title "Do pull-ups" :description "12-12-12" :created_time (cur-time)}
 			 {:title "Buy some milk" :created_time (cur-time)}
 			 {:title "Pick a movie for the weekend" :description "Maybe MiB3 or something" :created_time (cur-time)}])))
+
+(defn insert-sample-tasktags []
+	(let [pullups-task (first (select task (where {:title [like "%pull-ups"]})))
+		  health-tag (first (select tag (where {:tag "health"})))]
+		(if (or (nil? pullups-task) (nil? health-tag))
+			(throw (Exception. "Task or tag not found"))
+			(insert tasktag
+				(values
+					[{:task_id (:task_id pullups-task)
+					  :tag_id (:tag_id health-tag)}])))))
+
+(defn insert-demo-data []
+	(do
+		(delete tasktag)
+		(delete task)
+		(delete tag)
+		(insert-sample-tags)
+		(insert-sample-tasks)
+		(insert-sample-tasktags)))
