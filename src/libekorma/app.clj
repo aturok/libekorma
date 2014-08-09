03f15e77d74179fd3c0422aa283d16c024a2124a
Alex Turok <a.o.turok@gmail.com>
2014-08-10 00:25:46 +0400

    implemented tasks POST

--- src/libekorma/app.clj
+++ src/libekorma/app.clj
@@ -1,16 +1,33 @@
 (ns libekorma.app
 	(:require [compojure.core :refer [defroutes ANY]]
 			  [liberator.core :refer [defresource]]
-			  [korma.core :refer [select]]
+			  [korma.core :refer [select insert values]]
 			  [korma.db :refer [defdb]]
 			  [libekorma.util :as util]
 			  [libekorma.entities :refer [task]]))
 
 (defdb dbconnection util/dbcon)
 
+(defn error [message]
+	{:message message})
+
+(defn tasks-request-malformed?
+	[{{method :request-method} :request :as ctx}]
+	(if (= :post method)
+		(let [task-data (util/parse-json-body ctx)]
+			(if (empty? (:title task-data))
+				[true {:message "Task title missing or empty"}]
+				[false {:task-data task-data}]))
+		false))
+
 (defresource tasks-r
 	:available-media-types ["application/json"]
-	:allowed-methods [:get]
+	:allowed-methods [:get :post]
+	:malformed? tasks-request-malformed?
+	:post!
+		(fn [{task-data :task-data}]
+			(let [data (into task-data {:created_time (util/cur-time)})]
+				(insert task (values data))))
 	:handle-ok (fn [_]
 				(util/to-json
 					(select task))))
--- src/libekorma/util.clj
+++ src/libekorma/util.clj
@@ -1,6 +1,7 @@
 (ns libekorma.util
 	(:require [korma.db :refer [postgres]]
-			  [clojure.data.json :as json])
+			  [clojure.data.json :as json]
+			  [clojure.java.io :as io])
 	(:import [java.util Date]
        		 [java.sql Timestamp]))
 
@@ -15,3 +16,18 @@
   json/JSONWriter
   (-write [date out]
     (json/-write (str date) out)))
+
+(defn body-as-string [ctx]
+  (if-let [body (get-in ctx [:request :body])]
+    (condp instance? body
+      java.lang.String body
+      (slurp (io/reader body)))))
+
+(defn keywordify [dict]
+	(into {}
+		(map (fn [[k v]] [(keyword k) v]) dict)))
+
+(defn parse-json-body [context]
+	(if-let [body (body-as-string context)]
+		(keywordify (json/read-str body))
+		{}))
